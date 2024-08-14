e6db72781b00fa232668311ec1a7b2e68966109a
khoann2692002 <khoann2692002@gmail.com>
2024-08-14 16:55:43 +0700

    update dashboard

--- src/App.js
+++ src/App.js
@@ -52,15 +52,16 @@ import BlogDetail from "./pages/blog/BlogDetail";
 import Filter from "./pages/filter/Filter";
 import FeedbackModal from "./pages/booking/FeedbackModal";
 import Footer from "./pages/footer/Footer";
-import UserCard from "./pages/booking/UserCard"
+import UserCard from "./pages/booking/UserCard";
 import ChatWithFirebase from "./pages/chatWithFirebase/ChatWithFirebase";
-
 
 import MapboxSearchPopUp from "./pages/filter/MapboxSearchPopUp";
 import GeneralPolicy from "./pages/privacy/GeneralPolicy";
 import Regulations from "./pages/privacy/Regulations";
 import PrivacyPolicy from "./pages/privacy/PrivacyPolicy";
 import Complaints from "./pages/privacy/Complaints";
+import DashboardForAdmin from "./pages/dashboard/DashboardForAdmin";
+import DashboardForLessor from "./pages/dashboard/DashboardForLessor";
 
 function App() {
   const location = useLocation();
@@ -69,7 +70,8 @@ function App() {
   return (
     <>
       <Header />
-      <Routes><Route path="/userCard/:userId" element={<UserCard />} />
+      <Routes>
+        <Route path="/userCard/:userId" element={<UserCard />} />
         <Route path="/employee" element={<PostUser />} />
         <Route path="/employee/:id" element={<UpdateUser />} />
         <Route path="/login" element={<Login />} />
@@ -115,7 +117,14 @@ function App() {
           <Route path="/menu/profile" element={<Profile />} />
           <Route path="/menu/approveLicense" element={<ApproveLicense />} />
           <Route path="/menu/userData" element={<UserData />} />
-          <Route path="/menu/dashboard" element={<Dashboard />} />
+          <Route
+            path="/menu/dashboardForAdmin"
+            element={<DashboardForAdmin />}
+          />
+          <Route
+            path="/menu/dashboardForLessor"
+            element={<DashboardForLessor />}
+          />
         </Route>
 
         <Route path="/booking" element={<Booking />} />
