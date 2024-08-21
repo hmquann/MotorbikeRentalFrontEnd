909e037bfbab0702abc946746538cb795f3216bf
khoann2692002 <khoann2692002@gmail.com>
2024-08-21 16:23:34 +0700

    update dpl

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://rentalmotorbikewebapp.azurewebsites.net",
+  baseURL: "https://mimotorbe-htccgvbqdzb6h2em.eastus2-01.azurewebsites.net",
   //baseURL: "http://localhost:8080",
 });
 
